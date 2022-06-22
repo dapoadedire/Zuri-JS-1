86df3f9541481d5edc7b52744b444ed132de46d8
dapoadedire <adedireadedapo19@gmail.com>
2022-06-22 21:04:25 +0100

    Last last

--- script.js
+++ script.js
@@ -2,6 +2,4 @@ const myName = "Dapo Adedire";
 const myHeight = "190 cm";
 const myCountry = "Nigeria";
 
-alert("My name is " + myName)
-alert("My height is " + myHeight)
-alert("My country is " + myCountry)
+alert("My name is " + myName+ "\nMy height is " + myHeight + "\nMy country is " + myCountry)
